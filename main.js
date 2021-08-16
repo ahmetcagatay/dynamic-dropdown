68bd3b961113beed3b444122b010919f61ac5743
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2021-08-16 20:12:16 +0300

    recursive get function added and fixed

--- main.js
+++ main.js
@@ -1,58 +1,75 @@
-option1 = {
-    id: "1",
-    text: "option1",
-    paramaters: {
-        x: "5",
-        y: "10"
-    },
-    options: {
-        option11: {
-            id: "11",
-            text: "option11",
-            paramaters: {
-                a: "2",
-                b: "3"
-            }
-        },
+class Option {
+    constructor(path, text, parameters) {
+        this.path = path;
+        this.text = text;
+        this.parameters = parameters;
+        this.childs = [];
+    }
+    createChild(text, parameters) {
+        this.childs.push(new Option(this.path + "/" + this.childs.length, text, parameters));
+        return this.childs[this.childs.length - 1]
+    }
+    getChild(path){
+        var child = this;
+        var pathArr = path.split("/");
+        pathArr.shift();
+        console.log(pathArr.length);
+        if (pathArr.length > 0){
+            return child.childs[pathArr[0]].getChild(pathArr.join('/'));
+        }
+        else{
+            return child;
+        }
+        
+    }
+}
 
-        option12: {
-            id: "12",
-            text: "option12",
-            paramaters: {
-                c: "1",
-                d: "2"
-            }
+let base = new Option("0", "Merhaba", ["a", "b", "c"]);
+firstchild = base.createChild("Selamun", ["e", "d", "f"]);
+firstchild.createChild("Aleykum", ["e", "d", "f"]);
+selam = firstchild.createChild("Aleykum Selam", ["e", "d", "f"]);
+selam.createChild("Verahmetullahi", ["e", "d", "f"]);
+secondchild = base.createChild("nasılsın?", ["e", "d", "f"]);
+cevap = secondchild.createChild("iyiyim?", ["e", "d", "f"]);
+newBase = base.getChild("0");
+console.log(firstchild.path);
+console.log(newBase.path);
+
+function createDropwdownDiv(select,path) {
+    var newBase = base.getChild(path);
+    debugger;
+    $div = select.parent().append($('<div class="' + $option.attr("class") + '">'));
+    debugger;
+    $div = $div.children("." + $option.attr("class")).text(newBase.text).append($('<select id="' + newBase.path +  '"class="' + $option.attr("class") + '">'));
+    debugger;
+    $select = $div.children(":last-child");
+    for (const [key, value] of Object.entries(newBase.childs)) {
+        if (value.childs.length > 0){
+            $select.append($('<option class="dynamic">').val(value.path).text(value.text));
+        }
+        else{
+            $select.append($('<option class="static">').val(value.path).text(value.text));
+
         }
     }
 }
 
 
-const obj = option1;
+// console.log(firstchild.text);
 
-console.log(obj);
+// base.getChild("0/1");
 
-$(document).on("change", "select.base", function () {
+$(document).on("change", "select", function () {
 
     $option = $(this).children("option:selected");
 
     //delete last child for update
-    console.log($(this).siblings("div").remove());
+    // $(this).siblings("div").remove();
     //if option aa
-    console.log($option.attr("class"));
-    console.log("aa bb");
-    console.log($option.attr("class") == "aa bb");
+    // console.log($option.attr("class"));
 
-    //
-    if ($option.attr("class") == "aa") {
-        var el = $(this);
-        $div = el.parent().append($('<div class="' + $option.attr("class") + '">'));
-        $div = $div.children("." + $option.attr("class")).text("some").append($('<select class="' + $option.attr("class") + '">'));
-
-        // add options
-        var options = { x: "X", y: "Y" }
-        $select = $div.children(":last-child");
-        for (const [key, value] of Object.entries(options)) {
-            $select.append($('<option>').val(key).text(value));
-        }
+    if ($option.attr("class") == "dynamic") {
+        
+        createDropwdownDiv($(this),$option.val());
     }
 });
