09775423fce6ac342d8dc6e6e37b26fb47ac7dfb
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2021-08-17 23:48:24 +0300

    fixed child names

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 class Option {
-    constructor(text, parameters, path="0") {
-        this.path = path;
+    constructor(text, parameters = [], path = "0") {
+
         this.text = text;
         this.parameters = parameters;
+        this.path = path;
         this.childs = [];
     }
     createChild(text, parameters) {
-        this.childs.push(new Option(text, parameters,this.path + "/" + this.childs.length));
+        this.childs.push(new Option(text, parameters, this.path + "/" + this.childs.length));
         return this.childs[this.childs.length - 1]
     }
-    getChild(path){
+    getChild(path) {
         var child = this;
         var pathArr = path.split("/");
         pathArr.shift();
         console.log(pathArr.length);
-        if (pathArr.length > 0){
+        if (pathArr.length > 0) {
             return child.childs[pathArr[0]].getChild(pathArr.join('/'));
         }
-        else{
+        else {
             return child;
         }
 
@@ -26,22 +27,22 @@ class Option {
 
 
 
-function createDropwdownDiv(select,path) {
+function createDropwdownDiv(select, path) {
     var newBase = base.getChild(path);
 
     $div = select.parent().append($('<div class="' + $option.attr("class") + '">')).val(path);
-    $div.children("div."+$option.attr("class")).append("<label>").text(newBase.text);
-    $div = $div.children("."+$option.attr("class")).append($('<select id="' + newBase.path +  '"class="' + $option.attr("class") + '">'));
+    $div.children("div." + $option.attr("class")).append("<label>").text(newBase.text);
+    $div = $div.children("." + $option.attr("class")).append($('<select id="' + newBase.path + '"class="' + $option.attr("class") + '">'));
 
     //Options added into select
     $select = $div.children(":last-child");
     //Options added into select
-    $select.append($('<option class="dynamic">').val("...").text("Seçiniz"));
+    $select.append($('<option class="static">').text("Choise"));
     for (const [key, value] of Object.entries(newBase.childs)) {
-        if (value.childs.length > 0){
+        if (value.childs.length > 0) {
             $select.append($('<option class="dynamic">').val(value.path).text(value.text));
         }
-        else{
+        else {
             $select.append($('<option class="static">').val(value.path).text(value.text));
         }
     }
@@ -52,38 +53,36 @@ function createDropwdownDiv(select,path) {
 
 
 $(document).on("change", "select", function () {
-    
+
     $option = $(this).children("option:selected");
 
     //delete last child for update
     $(this).siblings("div").remove();
     //if option aa
     // console.log($option.attr("class"));
-    if ($option.attr("class") == "dynamic" && $option.val() != "...") {
+    // && $option.val() != "..."
+    if ($option.attr("class") == "dynamic") {
 
-        createDropwdownDiv($(this),$option.val());
+        createDropwdownDiv($(this), $option.val());
     }
 });
 
 
-let base = new Option("Merhaba", ["a", "b", "c"]);
+let base = new Option("Event");
 
-firstchild = base.createChild("Selamun Aleykum", ["e", "d", "f"]);
-aleykum = firstchild.createChild("Aleykum Selam", ["e", "d", "f"]);
-aleykum.createChild("Bitti", ["e", "d", "f"]);
-aleykum.createChild("Bitti2", ["e", "d", "f"]);
-aleykum.createChild("Bitti3", ["e", "d", "f"]);
-selam = firstchild.createChild("Ve Aleykum Selam", ["e", "d", "f"]);
-selam.createChild("Verahmetullahi", ["e", "d", "f"]);
+onMessage = base.createChild("On Message");
+    
+    sendMessage = onMessage.createChild("Send Message");
+    
+    kickMember = onMessage.createChild("Ban Member");
+        kickMember.createChild("Ban");
+        kickMember.createChild("Perma Ban");
 
-secondchild = base.createChild("nasılsın?", ["e", "d", "f"]);
-cevap = secondchild.createChild("iyiyim?", ["e", "d", "f"]);
-secondchild.createChild("Sen nasılsın?", ["e", "d", "f"]);
-secondchild.createChild("Nasıl gidiyor", ["e", "d", "f"]);
+onVoiceUpdate = base.createChild("On Voice Update");
+    onVoiceUpdate.createChild("Add Role");
+    onVoiceUpdate.createChild("Send Message");
+    onVoiceUpdate.createChild("Mute");
 
-newBase = base.getChild("0");
-console.log(firstchild.path);
-console.log(newBase.path);
 
 // $(document).ready( function(){
 //     createDropwdownDiv($("body"),"0");
